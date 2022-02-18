ceb553e6d3032ab06c4260d79397ec781b33beea
jollygrace15 <jollygrace.c.4@gmail.com>
2022-02-18 10:19:07 +0000

    created an update.hbs, including a route for it

---
+++ forms/index.js
@@ -0,0 +1,59 @@
+// import in caolan forms
+const forms = require("forms");
+// create some shortcuts
+const fields = forms.fields;
+const validators = forms.validators;
+
+var bootstrapField = function (name, object) {
+    if (!Array.isArray(object.widget.classes)) { object.widget.classes = []; }
+
+    if (object.widget.classes.indexOf('form-control') === -1) {
+        object.widget.classes.push('form-control');
+    }
+
+    var validationclass = object.value && !object.error ? 'is-valid' : '';
+    validationclass = object.error ? 'is-invalid' : validationclass;
+    if (validationclass) {
+        object.widget.classes.push(validationclass);
+    }
+
+    var label = object.labelHTML(name);
+    var error = object.error ? '<div class="invalid-feedback">' + object.error + '</div>' : '';
+
+    var widget = object.widget.toHTML(name, object);
+    return '<div class="form-group">' + label + widget + error + '</div>';
+};
+
+
+//allows Caolan forms to work with bootstrap
+//const bootstrapField = function (name, object) {
+
+//};
+
+const createProductForm = function(){
+    return forms.create({
+        "name":fields.string({
+            'required':true,
+            'errorAfterField': true,
+            'cssClasses': {
+                label: ['form-label']
+            }
+        }),
+        'cost': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            },
+            'validators':[validators.integer(), validators.min(1)]
+        }),
+        'description': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+    })
+}
+module.exports = {bootstrapField, createProductForm}
--- routes/products.js
+++ routes/products.js
@@ -4,13 +4,70 @@ const router = express.Router(); // #1 - Create a new express Router
 const { Product } = require('../models')
 //  #2 Add a new route to the Express router
 
+//import in createProductForm and bootstrapField
+const {bootstrapField, createProductForm} = require('../forms')
+
+
 router.get('/products', async function (req,res) {
     let products = await Product.collection().fetch();
-    res.send(products.toJSON());
+    //res.send(products.toJSON());
+    res.render('products/index',{
+        'products': products.toJSON() // make sure to call .toJSON()
+    })
 })
 
 router.get('/create', (req,res) =>{
-    res.send('Creating a new product')
+    //res.send('Creating a new product')
+    const productForm = createProductForm();
+    //convert the form to bootstrap design
+    res.render('products/create', {
+        'form': productForm.toHTML(bootstrapField)
+    })
+})
+
+router.post('/create', function(req,res){
+    //goal: create a new product based on the input in the form
+    const productForm = createProductForm();
+    productForm.handle(req,{
+        'success':async function(form){
+            console.log(form.data)
+            const newProduct = new Product();
+            newProduct.set('name', form.data.name);
+            newProduct.set('cost', form.data.cost);
+            newProduct.set('description', form.data.description);
+            await newProduct.save();
+            res.redirect('/products');
+        },
+        'error':function(form) {
+            res.render('products/create',{
+                'form': form.toHTML(bootstrapField)
+            })
+        }
+    })
+})
+
+router.get('/:product_id/update', async (req, res) => {
+    // retrieve the product
+    const productId = req.params.product_id
+    const product = await Product.where({
+        'id': productId
+    }).
+    fetch({
+        require: true
+    });
+
+    const productForm = createProductForm();
+
+    // fill in the existing values
+    productForm.fields.name.value = product.get('name');
+    productForm.fields.cost.value = product.get('cost');
+    productForm.fields.description.value = product.get('description');
+
+    res.render('products/update', {
+        'form': productForm.toHTML(bootstrapField),
+        'product': product.toJSON()
+    })
+
 })
 
 module.exports = router; // #3 export out the router
