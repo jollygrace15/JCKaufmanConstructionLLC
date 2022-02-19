f6591556831f3449648a58b221605036ca7a554b
jollygrace15 <jollygrace.c.4@gmail.com>
2022-02-19 16:14:30 +0000

    Was able to retrieve data for .get route

--- routes/products.js
+++ routes/products.js
@@ -14,8 +14,9 @@ async function getProductById(productId) {
     }).fetch({
         'require':true
     })
-    //return product
-}
+    return product
+};
+
 //working
 router.get('/products', async function (req,res) {
     let products = await Product.collection().fetch();
@@ -55,17 +56,20 @@ router.post('/create', function(req,res){
     })
 })
 
-router.get('/:product_id/update', async function(req, res){
+
+
+router.get('/products/:product_id/update', async function(req, res){
     //retrieve the product
     const productId = req.params.product_id;
-    //const product = await Product.where({
-     //   'id': productId
-     //    }).fetch({
-     //   'require': true
-    //})
-    const product = getProductById(productId)
+    const product = await Product.where({
+        'id': productId
+    }).fetch({
+        'require': true
+    })
+    //const product = getProductById(productId)
     const productForm = createProductForm();
-    res.send(product.toJSON());
+    //res.send("Editing " + productId)
+    //res.send(product.toJSON());
     // fill in the existing values
     productForm.fields.name.value = product.get('name');
     productForm.fields.cost.value = product.get('cost');
@@ -74,37 +78,44 @@ router.get('/:product_id/update', async function(req, res){
     res.render('products/update', {
         'form': productForm.toHTML(bootstrapField),
         'product': product.toJSON()
+    
     })
 })
 
 
-//router.post('/:product_id/update', async (req, res) => {
+router.post('products/:product_id/update', async function(req,res){
+    // fetch the instance of the product that we wish to update
+    const product = await Product.where({
+        'id': req.params.product_id
+    }).fetch({ 
+        require: true
+    })
+    // create the product form
+    const productForm = createProductForm();
+    // pass the request into the product form
+    productForm.handle(req, {
+        'success':async function(form){
+            // executes if the form data is all valid
+             product.set('name', form.data.name);
+             product.set('cost', form.data.cost);
+             product.set('description', form.data.description)
+            // if ALL the names of the fields matches
+            // the column names in the table, we can use the following shortcut
+            //product.set(form.data);
+            await product.save();
+            res.redirect('/products');
+        },
+        'error':function(form) {
+            // executes if the form data contains
+            // invalid entries
+            res.render('products/update', {
+                'form': form.toHTML(bootstrapField),
+                'product': product.toJSON()
+            })
 
-    // fetch the product that we want to update
-    //const product = await Product.where({
-    //    'id': req.params.product_id
-    //}).fetch({
-    //    require: true
-    //});
-
-
-
-    // process the form
- //   const productForm = createProductForm();
- //   productForm.handle(req, {
- //       'success': async (form) => {
- //           product.set(form.data);
- //           product.save();
- //           res.redirect('/products');
- //       },
- //       'error': async (form) => {
- //           res.render('products/update', {
- //               'form': form.toHTML(bootstrapField),
-   //             'product': product.toJSON()
-     //       })
-       // }
-    //})
-//})
+        }
+    })
+})
 
 
 //router.get('/:product_id/delete', async function(req, res){
