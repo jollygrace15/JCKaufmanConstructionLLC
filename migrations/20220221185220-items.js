2d7e6148a4cf0d41640fff24ad99e5fdf8c01a96
jollygrace15 <jollygrace.c.4@gmail.com>
2022-02-21 19:04:01 +0000

    cannot add foreign key, tried making a new table, but we cannot push.

---
+++ migrations/20220221185220-items.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  //first argument: name of the table
+  //second argument: an object representing the columns
+
+  //Create Table using Javascript instead of Mysql syntax
+  return db.createTable('products',{
+    "id": { "type": 'int', "primaryKey":true, "autoIncrement":true, "unsigned": true},
+    "name": { "type": 'string', "length":100, "notNull":true},
+    "cost": 'int',
+    "description":'text',
+    "brands":{
+      "id":
+      {
+        'type':'int',
+        'unsigned': true,
+        'primaryKey': true,
+        'autoIncrement': true,
+      },
+      "name":
+      {
+        "type":"string",
+        "length":100,
+        "notNull":true
+      }
+    },
+})
+};
+
+exports.down = function(db) {
+  return db.dropTable('products');
+};
+
+exports._meta = {
+  "version": 1
+};
